7190e4506e2506a39d01a0fc0a39948fd40041b8
Harsh Gupta <harshrp94@gmail.com>
2018-02-17 10:57:18 +0000

    Changing dependency in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     include_package_data=True,
     install_requires=['flask'],
     dependency_links=[
-        'git+https://github.com/harshrpg/COMP30670_CookieCutter.git@master#egg=systeminfo-0.1'],
+        'git+https://github.com/harshrpg/COMP30670_CookieCutter.git@master#egg=sysinfo-0.1'],
     enrty_points='''
         [flask.commands]
         run_app=run:main
