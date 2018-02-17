85bc027ef808065ae09d88d02b6a99e166961482
Harsh Gupta <harshrp94@gmail.com>
2018-02-17 11:45:08 +0000

    setup changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='systeminfo_flask',
     packages=['app'],
     include_package_data=True,
-    install_requires=['flask','systeminfo>=0.0'],
     dependency_links=[
-        'https://github.com/harshrpg/COMP30670_CookieCutter.git@master#egg=systeminfo-0.1']
+        'https://github.com/harshrpg/COMP30670_CookieCutter.git@master#egg=systeminfo-0.1'],
+    install_requires=['flask','sysinfo']
 )
