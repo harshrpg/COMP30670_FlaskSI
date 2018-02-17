da4e59854fd9b39158e443d7ead006e6957e0a75
Harsh Gupta <harshrp94@gmail.com>
2018-02-17 11:25:55 +0000

    Setup changes

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
+    version=0.1,
     name='systeminfo_flask',
     packages=['app'],
     include_package_data=True,
-    install_requires=['flask'],
+    install_requires=['flask','systeminfo'],
     dependency_links=[
-        'https://github.com/harshrpg/COMP30670_CookieCutter/tarball/master#egg=systeminfo-0.1']
+        'https://github.com/harshrpg/COMP30670_CookieCutter/tarball/master#egg=systeminfo']
 )
